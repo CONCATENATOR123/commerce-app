56ca7216a45a9fd9c3b291415d3edb1bc9234674
DANSO DERRICK BUABENG <drickdricky5@gmail.com>
2021-11-16 21:42:29 +0000

    added the product details page

--- App.js
+++ App.js
@@ -1,21 +1,54 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import loginScreen from './screens/loginScreen';
+import homeScreen from './screens/homeScreen';
+
+import cart from './screens/cart';
+import productDetails from './screens/productDetails';
+
+const Stack = createNativeStackNavigator();
+
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return ( <
+        NavigationContainer >
+        <
+        Stack.Navigator >
+        <
+        Stack.Screen options = {
+            { headerShown: false } }
+        name = "Log in"
+        component = { loginScreen }
+        /> <
+        Stack.Screen options = {
+            { headerShown: false } }
+        name = "Home"
+        component = { homeScreen }
+        />
+
+        <
+        Stack.Screen options = {
+            { headerShown: false } }
+        name = "cart"
+        component = { cart }
+        /> <
+        Stack.Screen options = {
+            { headerShown: false } }
+        name = "Details"
+        component = { productDetails }
+        /> <
+        /Stack.Navigator> <
+        /NavigationContainer>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+
 });
